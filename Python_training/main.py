56f4ec52256574cf9b4335b5c7cb933524773dbf
Nick Fard <stavakol@ucsd.edu>
2014-09-14 23:52:37 -0700

    updating python training files

--- Python_training/main.py
+++ Python_training/main.py
@@ -3,6 +3,11 @@
 #In order to call the functions after they called we have to use main functon
 def main():
 	print('Calling function before it defined')
+	a = 'one'
+#In order to find the type of variabel we should use tyep(variable) function
+	print('A is :',type(a))
+	b = 5
+	print('B is :',type(b))
 	test()
 		
 def test():
