cfa419ada68320010191bf4c89295ae47fda37f8
Nick Fard <stavakol@ucsd.edu>
2015-01-02 21:14:41 -0800

    Adding installer to the package Fix#3

---
+++ dist/KeplerMagic-1.0/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# encoding: UTF-8
+
+import sys
+import os
+import shutil
+import platform
+
+
+
+from distutils.core import setup
+from KeplerMagicLib import Kepler_Magic
+
+desc = """\
+iPython Kepler Magic Function
+==============
+This pakcge will enhase ipython to use kepler workflows.
+
+Kepler website:
+https://kepler-project.org/
+
+More inforamtion about this package:
+https://github.com/hamidre13/IPython-Kepler-Magic-Function
+
+This package is property of Words Group San Diego Super Computer Center:
+http://words.sdsc.edu/
+"""
+setup(name='KeplerMagic',
+      version='1.0',
+      author  ='Hamid Tavakoli',
+      author_email= 'stavakol@ucsd.edu',
+      url='https://github.com/hamidre13/IPython-Kepler-Magic-Function',
+      long_description=desc,
+      py_modules=['KeplerMagicLib','KeplerMagicFunction'],
+      )
