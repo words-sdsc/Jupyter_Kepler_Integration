cb96df9c6b5077d20871443a42312cc0e2390e92
Alok Singh <a1singh@ucsd.edu>
2015-08-06 10:51:13 -0700

    mian to main

--- KeplerMagicLib.py
+++ KeplerMagicLib.py
@@ -47,4 +47,4 @@ class Kepler_Magic():
 
 #We do not have main function in this file,
 #this line is here in case main will be added in future
-if __name__ == "__mian__":main()
+if __name__ == "__main__":main()
